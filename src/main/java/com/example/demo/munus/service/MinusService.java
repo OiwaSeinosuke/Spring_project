932d554f7d02fac236d0baaf49cf4ba1f26f2de9
DESKTOP-0EV1QAR\qzpo1 <qzpo1109@gmail.com>
2023-10-11 23:21:24 +0900

    first commit

---
+++ src/main/java/com/example/demo/munus/service/MinusService.java
@@ -0,0 +1,5 @@
+package com.example.demo.munus.service;
+
+public class MinusService {
+
+}
---
+++ src/main/java/practice/PracticeWhile.java
@@ -0,0 +1,10 @@
+package practice;
+public class PracticeWhile {
+	public void countDown() {
+	int num = 0;
+	int dec = 100;
+	
+	while (dec-- >= 0) {
+		System.out.println(dec);
+	}}
+}
